2291202a37d270e7d07b8fc78762900b72ceea87
Will Moritz <will@crowdfavorite.com>
2012-10-28 19:17:29 -0600

    added captions to candidate pictures

--- db/schema.rb
+++ db/schema.rb
@@ -28,8 +28,6 @@ ActiveRecord::Schema.define(:version => 20121028202935) do
     t.string   "name"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-    t.string   "email"
-    t.integer  "post_id"
     t.string   "fingerprint"
   end
 
